6cd649a2bb7a44d0e937e9e163ed05255fff0097
jin501 <soljin01@gmail.com>
2016-10-15 14:19:22 -0400

    users controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
   get '/' do
-    binding.pry
-    if logged_in?
-      @user = current_user
-      redirect to "/users/#{@user.slug}"
-    else
-      erb :'/users/index'
-    end
+    # binding.pry
+    # if logged_in?
+    #   @user = current_user
+    #   redirect to "/users/#{@user.slug}"
+    # else
+    #   erb :'/users/index'
+    # end
+    erb :'/users/index'
   end
 
   post '/signup' do
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
       redirect to '/login'
     # elsif existing_user.class == NilClass
     else
-      user = User.create(username: params[:username], password: params[:password], full_name: params[:full_name], email: params[:email])
+      user = User.create(username: params[:username], password: params[:password], name: params[:name], email: params[:email])
       user.save
       session[:user_id] = user.id
       ##flash message 'new user successfully created'
@@ -29,14 +30,27 @@ class UsersController < ApplicationController
 
   get '/login' do
     if logged_in?
-      redirect to '/buckets'
+      redirect to "/users/#{@user.slug}"
     else
       erb :'/users/login'
     end
   end
 
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/users/#{@user.slug}"
+    else
+      ##flash message "wrong credentials, please try again"
+      redirect to '/login'
+    end
+  end
 
+  get '/users/:slug' do
+    erb :profile
+  end
 
-
+  post '/users'
 
 end
